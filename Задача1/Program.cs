80f28fe76c3fef670607cf46aad65620df9de741
Dmitriy <klimov.dima@mail.ru>
2022-07-09 12:10:08 +0300

    Нахождение максимального и минимального числа

---
+++ Задача1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите число 1: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+if (number_2 == number_1)
+{
+Console.WriteLine(" Введено неверное число");
+}
+else if (number_2 > number_1)
+
+{
+    Console.WriteLine(number_2  + " - Наибольшее число");
+    Console.WriteLine(number_1  + " - Наименьшее число");
+}
+else
+{
+    Console.WriteLine(number_1  + " - Наибольшее число");
+    Console.WriteLine(number_2  + " - Наименьшее число");
+}
+
