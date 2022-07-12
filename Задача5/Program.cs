ad6f2b07dad7777ef20c90b7bfb3eda17ef2101c
Dmitriy <klimov.dima@mail.ru>
2022-07-12 22:39:56 +0300

    Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа

---
+++ Задача5/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+void Zadacha5()
+{
+    Console.Write("Введите трехзначное число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    int tens = number / 10;
+    int hungs = number / 100;
+
+    int resultNumber = tens - hungs * 10;
+
+    Console.WriteLine("Вторая цифра трехзначного числа = " + resultNumber);
+}
+
+Zadacha5();
