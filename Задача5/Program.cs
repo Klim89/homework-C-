6e8247eff1cf28cac080f9d7f3c188b9e7d7a7c5
Dmitriy <klimov.dima@mail.ru>
2022-07-13 11:04:00 +0300

    Добавил проверку на 3-х значность

--- Задача5/Program.cs
+++ Задача5/Program.cs
@@ -9,13 +9,18 @@ void Zadacha5()
 {
     Console.Write("Введите трехзначное число: ");
     int number = Convert.ToInt32(Console.ReadLine());
-
+    
     int tens = number / 10;
     int hungs = number / 100;
 
+    if (number > 100 && number < 1000)
+    {
     int resultNumber = tens - hungs * 10;
-
     Console.WriteLine("Вторая цифра трехзначного числа = " + resultNumber);
+    }
+    else 
+    {
+        Console.WriteLine("Введите трехзначное число");
+    }
 }
-
 Zadacha5();
