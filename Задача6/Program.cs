9c3ffb9d457c1e3cf23e05f7b2b8170949eb90b7
Dmitriy <klimov.dima@mail.ru>
2022-07-14 21:23:40 +0300

    Добавил функицю определения остатка от деления для нахождения 3 цифры

--- Задача6/Program.cs
+++ Задача6/Program.cs
@@ -5,7 +5,7 @@
 32679 -> 6
 */
 
-void Zadacha6()
+/*void Zadacha6()
 {
     Console.Write("Введите число: ");
     int number = Convert.ToInt32(Console.ReadLine());
@@ -21,4 +21,31 @@ void Zadacha6()
     }
 }
 Zadacha6();
+*/
+
+void Zadacha6_1()
+{
+    Console.Write("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    if (number > 99)
+    {
+        int result = Temp(number);
+        Console.WriteLine("Третья цифра числа = " + result);
+    }
+    else
+    {
+        Console.WriteLine("Третья цифра отсутствует");
+    }
+}
+Zadacha6_1();
+
+
+int Temp(int number)
+{
+    while (number >= 1000) number = number / 10; 
+    int temp = number % 10;
+    return temp;
+}
+
 
